a9f05072cb6fb1fc50c91a8a3af365f93d24f05b
Nagarjun <w3.nagarjuna@gmail.com>
2020-10-20 18:36:01 +0530

    docs folder for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/picsum-vue-app/' : '/',
     configureWebpack: {
         devtool: 'source-map'
-    }
+    },
+    outputDir: path.resolve(__dirname, "docs"),
 }
